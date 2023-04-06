01b85f22cdc92056bc3c90ca8a08abbd124ba45a
AB-Akram <abdelhakakram79@gmail.com>
2023-04-06 17:45:13 +0200

    CRUD API FOR FootBallFieldType with generic-views (remove class FootBallField)

--- Field_type/models.py
+++ Field_type/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class FootBallField(models.Model):
     name = models.CharField(max_length=10)
 
+
 class FootBallFieldType(models.Model):
-    footBallField = models.ForeignKey(FootBallField, on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
     max = models.IntegerField()
     price = models.FloatField()
--- Field_type/serializer.py
+++ Field_type/serializer.py
@@ -2,23 +2,7 @@ from rest_framework import serializers, status
 from .models import *
 
 
-class FootBallFieldSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FootBallField
-        fields = ('id', 'name',)
-
-    def create(self, validated_data):
-        return FootBallField.objects.create(
-            name=validated_data['name'],
-
-        )
-
-
 class FootBallFieldTypeSerializer(serializers.ModelSerializer):
-    footBallField = FootBallFieldSerializer(required=True)
-
     class Meta:
         model = FootBallFieldType
-        fields = ('id', 'name', 'max', 'price', 'advance', 'footBallField',)
-
-    
+        fields = ('id', 'name', 'max', 'price', 'advance',)
