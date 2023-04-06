e70fb18260a568545da87f985da242ab5104125b
AB-Akram <abdelhakakram79@gmail.com>
2023-04-06 14:09:38 +0200

    CRUD API FOR FootBallFieldType with generic-views (update fields and remove create method in class FootBallFieldTypeSerializer)

--- Field_type/serializer.py
+++ Field_type/serializer.py
@@ -5,7 +5,7 @@ from .models import *
 class FootBallFieldSerializer(serializers.ModelSerializer):
     class Meta:
         model = FootBallField
-        fields = '__all__'
+        fields = ('id', 'name',)
 
     def create(self, validated_data):
         return FootBallField.objects.create(
@@ -19,14 +19,6 @@ class FootBallFieldTypeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = FootBallFieldType
-        fields = '__all__'
+        fields = ('id', 'name', 'max', 'price', 'advance', 'footBallField',)
 
-    def create(self, validated_data):
-        footBallField_data = validated_data.pop('footBallField')
-        footBallField = FootBallFieldSerializer.create(FootBallFieldSerializer(), validated_data=footBallField_data)
-        footBallfield, created = FootBallFieldType.objects.update_or_create(footBallField=footBallField,
-                                                                            name=validated_data.pop('name'),
-                                                                            max=validated_data.pop('max'),
-                                                                            price=validated_data.pop('price'),
-                                                                            advance=validated_data.pop('advance'))
-        return footBallfield
+    
