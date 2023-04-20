d865876ce72618972ba02f074ba7d7ed9e12249c
voidnowhere <alivnw@outlook.com>
2023-04-20 11:46:47 +0000

    Setup mysql db usage

--- FFRM_API/settings.py
+++ FFRM_API/settings.py
@@ -82,6 +82,13 @@ WSGI_APPLICATION = 'FFRM_API.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    } if config('PRODUCTION', cast=bool) else {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
